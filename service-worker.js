104b6a464993b9ec302c75ba2ed68e9c2c685440
samdedy <samgalaxy111111@gmail.com>
2020-11-23 20:34:02 +0700

    set WorkBox

--- service-worker.js
+++ service-worker.js
@@ -32,40 +32,64 @@ workbox.precaching.precacheAndRoute(
 	}
 );
 
+// Team-Icona Cache
 workbox.routing.registerRoute(
-  /\.(?:png|gif|jpg|jpeg|svg)$/,
-  workbox.strategies.cacheFirst({
-    cacheName: 'images',
+    new RegExp("https://crests\\.football-data\\.org.*\\.svg"),
+    workbox.strategies.cacheFirst({
+      cacheName: "team-icon",
+      plugins: [
+        new workbox.cacheableResponse.Plugin({
+          statuses: [0, 200],
+        }),
+        new workbox.expiration.Plugin({
+          maxAgeSeconds: 60 * 60 * 24 * 365,
+          maxEntries: 30,
+        }),
+      ],
+    })
+);
+  
+// API Cache
+workbox.routing.registerRoute(
+    new RegExp("https://api.football-data.org/v2/"),
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: "cache-api",
+})
+);
+  
+// Images Cache
+workbox.routing.registerRoute(
+/.*.(?:png|jpg|jpeg|svg|gif|crests.football-data.org)/,
+workbox.strategies.cacheFirst({
+    cacheName: "cache-image",
     plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 hari
-      }),
+    new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 7,
+        maxEntries: 50,
+        purgeOnQuotaError: true,
+    }),
     ],
-  }),
+})
 );
 
-// Menyimpan cache dari CSS Google Fonts
-workbox.routing.registerRoute(
-  /^https:\/\/fonts\.googleapis\.com/,
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: 'google-fonts-stylesheets',
-  })
-);
- 
-// Menyimpan cache untuk file font selama 1 tahun
-workbox.routing.registerRoute(
-  /^https:\/\/fonts\.gstatic\.com/,
-  workbox.strategies.cacheFirst({
-    cacheName: 'google-fonts-webfonts',
-    plugins: [
-      new workbox.cacheableResponse.Plugin({
-        statuses: [0, 200],
-      }),
-      new workbox.expiration.Plugin({
-        maxAgeSeconds: 60 * 60 * 24 * 365,
-        maxEntries: 30,
-      }),
-    ],
-  })
-);
+self.addEventListener('push', function(event) {
+    var body;
+    if (event.data) {
+      body = event.data.text();
+    } else {
+      body = 'Push message no payload';
+    }
+    var options = {
+      body: body,
+      icon: 'icon/icon.png',
+      vibrate: [100, 50, 100],
+      data: {
+        dateOfArrival: Date.now(),
+        primaryKey: 1
+      }
+    };
+    event.waitUntil(
+      self.registration.showNotification('Push Notification', options)
+    );
+});
+
